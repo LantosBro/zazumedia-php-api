0c7de70db23e3fc2faf3b851b5043af795c44a59
alex.stnv <alex.stnv@gmail.com>
2018-01-15 17:22:20 +0300

    zmtech

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once 'Zmtech.php';
 
 /**
  * Пример использования класса Zmtech.php
- * Для работы необходимо создать файл с токеном и указать к нему путь
  */
 
 try {
@@ -11,11 +10,61 @@ try {
     $id = '1';
     $key = '6Imh0dHBzOi8vZ28uemF6dW1lZGlhLnJ1L3Byb2ZpbGUvdG9rZW4i';
 
-    $zazu = new Zmtech($id, $key);
-    $info = $zazu->getInfo();
+    // Получить инфо об авторизованном аккаунте
+    $zmtech = new Zmtech( $id, $key );
+    $info = $zmtech->getInfo();
+    var_dump( $info );
 
-    var_dump($info);
+    // Отправить одно Sms
+    $response = $zmtech->sendSms( [
+        'phone' => '79112223344',
+        'message' => 'test sms',
+        'sender' => 'zmtech.ru'
+    ] );
 
-} catch( Exception $e ) {
+    // Отправить несоколько Sms (до 100 штук)
+    $response = $zmtech->sendSms( [
+        [
+            'phone' => '79112223344',
+            'message' => 'test sms',
+            'sender' => 'zmtech.ru'
+        ], [
+            'phone' => '79112223345',
+            'message' => 'test sms 2',
+            'sender' => 'zmtech.ru'
+        ]
+    ] );
+
+    // Отправить одно сообщение Viber
+    $response = $zmtech->sendViber( [
+        'phone' => '79112223344',
+        'message' => 'test viber',
+        'btntext' => 'zmtech.ru',
+        'btnlink' => 'http://zmtech.ru',
+        'picpath' => 'http://zmtech.ru/logo.png'
+    ] );
+
+    // Отправить несоколько сообщений Viber (до 100 штук)
+    $response = $zmtech->sendViber( [
+        [
+            'phone' => '79112223344',
+            'message' => 'test viber',
+            'btntext' => 'zmtech.ru',
+            'btnlink' => 'http://zmtech.ru',
+            'picpath' => 'http://zmtech.ru/logo.png'
+        ], [
+            'phone' => '79112223345',
+            'message' => 'test viber 2',
+            'btntext' => 'zmtech.ru',
+            'btnlink' => 'http://zmtech.ru',
+            'picpath' => 'http://zmtech.ru/logo.png'
+        ]
+    ] );
+
+    // Запросить статусы
+    $statuses = $zmtech->getStatuses();
+    var_dump( $statuses );
+
+} catch ( Exception $e ) {
     echo $e->getMessage();
 }
